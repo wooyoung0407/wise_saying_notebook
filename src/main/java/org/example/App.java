b1a71c5a72d2a5d45b03aac80d02fd14717dfa9b
wooyoung0407 <wooyoung0407@naver.com>
2023-03-30 20:18:19 +0900

    1단계

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while(true){
+            System.out.println("명령)");
+            String command = sc.nextLine().trim();
+
+            if(command.equals("종료")){
+                break;
+            }
+        }
+    }
+}
