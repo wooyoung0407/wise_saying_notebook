166fdd79354ee08af6231294ed7dd93a36f1a80e
깃wooyoung0407 <wooyoung0407@naver.com>
2023-04-04 23:43:15 +0900

    리팩토링X 실패

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,15 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
     private final Scanner sc;
 
-    public App(Scanner sc) {    
+
+    public App(Scanner sc) {
         this.sc = sc;
     }
 
@@ -15,40 +18,49 @@ public class App {
         int number = 0;
         ArrayList<WiseSaying> wiseSayings = new ArrayList<>(); //목록 파트 list 작성
 
-        while(true){ //while문으로 무한 반복문
+        while (true) { //while문으로 무한 반복문
             System.out.println("명언)");
             String command = sc.nextLine().trim(); // 명언)이후 문자열 받기, 공백 지우기 , 명언) 계속 반복
+            Rq rq = new Rq(command);
 
-            if(command.equals("종료")){ //문자열은 equals를 써서 같은지 확인 종료시 반복문 종료
+            if (command.equals("종료")) { //문자열은 equals를 써서 같은지 확인 종료시 반복문 종료
                 System.out.println("명언앱종료");
                 break;
-            }else if(command.equals("등록")){
+            } else if (command.equals("등록")) {
                 System.out.println("명언 : ");
                 String wise = sc.nextLine().trim(); // 명언받아오는 String
                 System.out.println("작가 : ");
                 String name = sc.nextLine().trim(); // 작가이름 받아오는 String
 //                System.out.println("1번 명언이 등록되었습니다.");
                 int id = ++number;
-                System.out.printf("%d번 명언이 등록되었습니다.\n",id);
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
 
-                WiseSaying wiseSaying = new WiseSaying(number,name,wise); //WiseSaying 매개변수 가져오기
+                WiseSaying wiseSaying = new WiseSaying(number, name, wise); //WiseSaying 매개변수 가져오기
 
                 wiseSayings.add(wiseSaying); // wiseSayings 리스트에 wiseSaying 의 매개변수 넣기
-            }else if(command.equals("목록")){
+            } else if (command.equals("목록")) {
                 System.out.println("명언 수 : " + wiseSayings.size()); // list의 길이및 수 찾기
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("=".repeat(30));
 
-                for(int i = wiseSayings.size()-1;i>=0;i--){ //for문을 돌려 최신 순으로 목록을 나타낸다.
+                for (int i = wiseSayings.size() - 1; i >= 0; i--) { //for문을 돌려 최신 순으로 목록을 나타낸다.
                     WiseSaying wiseSaying = wiseSayings.get(i);
 
-                    System.out.printf("%d / %s / %s\n",wiseSaying.getNumber(),wiseSaying.getName(),wiseSaying.getWise());
+                    System.out.printf("%d / %s / %s\n", wiseSaying.getNumber(), wiseSaying.getName(), wiseSaying.getWise());
                     // wiseSayings.get(i) , wiseSaying getter 를 각각 넣어 출력해준다.
                 }
+            } else if (command.equals("삭제")) {
+                int id = rq.getIntParam("id", -1);
+                if (id == -1) {
+                    System.out.println("정수를 입력해주세요.");
+                    return;
+                }
+                WiseSaying wiseSaying = findById(id);
+
+                System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
             }
-
-
         }
 
     }
 }
+
---
+++ src/main/java/org/example/Rq.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rq {
+    private String actionCode;
+    private Map<String,String> params;
+
+    public Rq(String command){
+
+        String[] commandBits = command.split("\\?",2);
+        String actionCode = commandBits[0];
+        params = new HashMap<>();
+
+        if(commandBits.length == 1)return;
+
+        String[] paramBits = commandBits[1].split("&");
+
+        for (String paramStr : paramBits){
+            String[] paramStrBits = paramStr.split("=",2);
+
+            if(paramStrBits.length == 1) continue;
+
+            String key = paramStrBits[0];
+            String value = paramStrBits[1];
+
+            params.put(key , value);
+        }
+    }
+    public String getActionCode(){
+        return this.actionCode;
+    }
+   public String getParam(String name){
+        return params.get(name);
+   }
+   public int getIntParam(String name,int defaultValue){
+        try{
+            return Integer.parseInt(getParam(name));
+        }catch (NumberFormatException e){
+
+        }
+        return  defaultValue;
+   }
+    private WiseSaying findById(int id) {
+        for (WiseSaying wiseSaying : wiseSayings){
+
+        }
+    }
+}
