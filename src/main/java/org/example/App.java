711978eb4448b337e5cec7454292ed9dc958c0e4
깃wooyoung0407 <wooyoung0407@naver.com>
2023-04-04 22:45:41 +0900

    2단계

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,8 +10,24 @@ public class App {
     }
 
     public void run() {
-        System.out.println("명언)");
-        String command = sc.nextLine().trim(); // 명언)이후 문자열 받기, 공백 지우기
+        System.out.println("== 명언 앱 ==");
+
+        while(true){ //while문으로 무한 반복문
+            System.out.println("명언)");
+            String command = sc.nextLine().trim(); // 명언)이후 문자열 받기, 공백 지우기 , 명언) 계속 반복
+
+            if(command.equals("종료")){ //문자열은 equals를 써서 같은지 확인 종료시 반복문 종료
+                System.out.println("명언앱종료");
+                break;
+            }else if(command.equals("등록")){
+                System.out.println("명언 : ");
+                String wise = sc.nextLine().trim(); // 명언받아오는 String
+                System.out.println("작가 : ");
+                String name = sc.nextLine().trim(); // 작가이름 받아오는 String
+            }
+
+
+        }
 
     }
 }
