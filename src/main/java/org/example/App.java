c4c631a2d1b705f6d4f7b06d9137cedbc59fb333
깃wooyoung0407 <wooyoung0407@naver.com>
2023-04-04 22:40:08 +0900

    1단계

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,48 +1,17 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     private final Scanner sc;
 
-    public App(Scanner sc) {
+    public App(Scanner sc) {    
         this.sc = sc;
     }
 
     public void run() {
-        System.out.println("== 명언 앱 ==");
-        int number = 0;
-        ArrayList<WiseSaying> wiseSayings = new ArrayList<>();
+        System.out.println("명언)");
+        String command = sc.nextLine().trim(); // 명언)이후 문자열 받기, 공백 지우기
 
-        while (true) {
-            System.out.println("명령)");
-            String command = sc.nextLine().trim();
-            if (command.equals("종료")) {
-                break;
-            } else if (command.equals("등록")) {
-
-                System.out.println("명언 : ");
-                String wise = sc.nextLine().trim();
-                System.out.println("작가 : ");
-                String name = sc.nextLine().trim();
-
-                int id = ++number;
-                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-
-                WiseSaying wiseSaying = new WiseSaying(number, name, wise);
-
-                wiseSayings.add(wiseSaying);
-            } else if (command.equals("목록")) {
-                System.out.println("명언 수 :" + wiseSayings.size());
-                System.out.println("번호 / 작가 / 명언\n");
-
-                for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-                    WiseSaying wiseSaying = wiseSayings.get(i);
-
-                    System.out.printf("%d / %s / %s \n", wiseSaying.getNumber(),wiseSaying.getName(),wiseSaying.getWise());
-                }
-            }
-        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //input 태그
 
-        new App(sc).run();
+        new App(sc).run(); //App 클래스에 Scanner 변수를 넣어 run시키겠다.
 
-        sc.close();
-        }
+        sc.close(); //Scanner 닫기
     }
+}
